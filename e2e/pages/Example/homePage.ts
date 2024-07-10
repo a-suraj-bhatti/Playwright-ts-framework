b1395db6a74de149cca44e7575bc4230c9fbf9f1
surajbhatti <a.suraj@outlook.com>
2024-07-10 12:13:07 +0530

    remove unnecessary examples

--- e2e/pages/Example/homePage.ts
+++ e2e/pages/Example/homePage.ts
@@ -1,17 +1,10 @@
 import { Locator, Page } from "@playwright/test";
-import LeftMenuComponent from "@e2e/pages/Example/components/leftMenuComponent";
-import TopMenuComponent from "@e2e/pages/Example/components/topMenuComponent";
 import { step } from "@e2e/libs/custom-decorators";
 
 class HomePage {
-  //left menu component and top menu component
 
-  private leftMenuComponent: LeftMenuComponent;
-  private topMenuComponent: TopMenuComponent;
 
   constructor(private readonly page: Page) {
-    this.leftMenuComponent = new LeftMenuComponent(page);
-    this.topMenuComponent = new TopMenuComponent(page);
   }
 
   readonly addTocartButton = (productName: string): Locator =>
