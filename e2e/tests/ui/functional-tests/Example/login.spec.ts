993cd02c2badff94f73d76a6aad242d8ee7f0341
surajbhatti <a.suraj@outlook.com>
2024-07-19 08:54:02 +0530

    enahnced report generatkr

--- e2e/tests/ui/functional-tests/Example/login.spec.ts
+++ e2e/tests/ui/functional-tests/Example/login.spec.ts
@@ -1,28 +1,50 @@
 import { expect, test } from "@e2e/fixtures/customFixtures";
 
-test.describe("There are two tests", { tag: "@functionality_describe" }, () => {
-  test(
-    "Verify that the user is able to login to the application",
-    { tag: ["@functionality_login_test", "@positive", "@priority_high"] },
-    async ({ loginPage, homePage }) => {
-      await loginPage.visit();
-      await loginPage.login();
-      await expect(homePage.product("Sauce Labs Backpack")).toBeVisible();
-    },
-  );
-
-  test(
-    "test login page",
-    {
-      annotation: {
-        type: "issue",
-        description: "https://github.com/microsoft/playwright/issues/23180",
+test.describe(
+  "There are two tests",
+  {
+    tag: ["@functionality_describe", "@negative"],
+    annotation: { type: "jira issue", description: "This is a test description" },
+  },
+  () => {
+    test(
+      "Verify that the user is able to login to the application",
+      { tag: ["@positive", "@priority_high", "@functionality_descoverride"] },
+      async ({ loginPage, homePage }) => {
+        await loginPage.visit();
+        await loginPage.login();
+        await expect(homePage.product("Sauce Labs Backpack")).toBeVisible();
       },
-      tag: "@functionality_singleTag",
+    );
+
+    test(
+      "test login page",
+      {
+        annotation: {
+          type: "issue",
+          description: "https://github.com/microsoft/playwright/issues/23180",
+        },
+        tag: "@priority_low",
+      },
+      async ({ page }) => {
+        // ...
+        page.goto("");
+      },
+    );
+  },
+);
+
+test(
+  "test login page22",
+  {
+    annotation: {
+      type: "issue",
+      description: "https://github.com/microsoft/playwright/issues/23180",
     },
-    async ({ page }) => {
-      // ...
-      page.goto("");
-    },
-  );
-});
+    tag: "@priority_low",
+  },
+  async ({ page }) => {
+    // ...
+    page.goto("");
+  },
+);
--- scripts/report-generator.js
+++ scripts/report-generator.js
@@ -8,10 +8,13 @@ const walk = require("acorn-walk");
 const testDirectory = "e2e/tests";
 
 function parseTags(tags, currentTest) {
+  if (!Array.isArray(tags)) {
+    tags = [tags]; // Convert single tag to array
+  }
   tags.forEach((tag) => {
     if (tag.startsWith("@functionality_")) {
-        const parts = tag.split('_');
-        currentTest.functionality = parts.slice(1).join('_');
+      const parts = tag.split('_');
+      currentTest.functionality = parts.slice(1).join('_');
     } else if (tag.startsWith("@priority_")) {
       currentTest.priority = tag.split("_")[1];
     } else if (tag === "@positive") {
@@ -22,43 +25,234 @@ function parseTags(tags, currentTest) {
   });
 }
 
-function extractDataFromFile(filePath, testType) {
-  console.log(filePath);
-  const data = fs.readFileSync(filePath, "utf-8");
-  const ast = parse(data, {
-    sourceType: "module",
-    ecmaVersion: 2020,
-    loc: true,
-    range: true,
-    tokens: true,
-  });
-  const testData = [];
+// function extractDataFromFile(filePath, testType) {
+//   console.log(filePath);
+//   const data = fs.readFileSync(filePath, "utf-8");
+//   const ast = parse(data, {
+//     sourceType: "module",
+//     ecmaVersion: "latest",  // Use the latest ECMAScript version
+//     loc: true,
+//     range: true,
+//     tokens: true,
+//     jsx: true,  // Enable JSX parsing if your tests use it
+//     ecmaFeatures: {
+//       globalReturn: false,
+//       impliedStrict: true,
+//       jsx: true,
+//     },
+//     // Enable all types of statements to be parsed
+//     allowImportExportEverywhere: true,
+//     allowAwaitOutsideFunction: true,
+//     allowReturnOutsideFunction: true,
+//     allowSuperOutsideMethod: true,
+//   });
+//   const testData = [];
 
-  walk.simple(ast, {
-    CallExpression(node) {
+//   walk.simple(ast, {
+//     CallExpression(node) {
+//         console.log(node);
+//       if (
+//         node.callee.name === "test" &&
+//         node.arguments.length >= 2 &&
+//         (node.arguments[0].type === "Literal" || node.arguments[0].type === "TemplateLiteral")
+//       ) {
+//         const testName = node.arguments[0].value || node.arguments[0].quasis[0].value.raw;
+//         const currentTest = { testName, functionality: "", testType, priority: "", scenarioType: "" };
+
+//         if (node.arguments[1].type === "ObjectExpression") {
+//           const tagProperty = node.arguments[1].properties.find((prop) => prop.key.name === "tag");
+//           if (tagProperty) {
+//             let tags;
+//             if (tagProperty.value.type === "ArrayExpression") {
+//               tags = tagProperty.value.elements.map((el) => el.value);
+//             } else if (tagProperty.value.type === "Literal") {
+//               tags = tagProperty.value.value;
+//             }
+//             console.log(tags)
+//             parseTags(tags, currentTest);
+//           }
+//         }
+
+//         testData.push(currentTest);
+//       }
+//     },
+//   });
+
+//   return testData;
+// }
+// function extractDataFromFile(filePath, testType) {
+//     console.log(filePath);
+//     const data = fs.readFileSync(filePath, "utf-8");
+//     const ast = parse(data, {
+//       sourceType: "module",
+//       ecmaVersion: "latest",
+//       loc: true,
+//       range: true,
+//       tokens: true,
+//     });
+//     const testData = [];
+//     let describeTags = {};
+  
+//     function processTestNode(node, inheritedTags = {}) {
+//       if (
+//         node.callee.name === "test" &&
+//         node.arguments.length >= 2 &&
+//         (node.arguments[0].type === "Literal" || node.arguments[0].type === "TemplateLiteral")
+//       ) {
+//         const testName = node.arguments[0].value || node.arguments[0].quasis[0].value.raw;
+//         const currentTest = { 
+//           testName, 
+//           functionality: inheritedTags.functionality || "",
+//           testType, 
+//           priority: inheritedTags.priority || "", 
+//           scenarioType: inheritedTags.scenarioType || "" 
+//         };
+  
+//         if (node.arguments[1].type === "ObjectExpression") {
+//           const tagProperty = node.arguments[1].properties.find((prop) => prop.key.name === "tag");
+//           if (tagProperty) {
+//             let tags;
+//             if (tagProperty.value.type === "ArrayExpression") {
+//               tags = tagProperty.value.elements.map((el) => el.value);
+//             } else if (tagProperty.value.type === "Literal") {
+//               tags = tagProperty.value.value;
+//             }
+//             parseTags(tags, currentTest);
+//           }
+//         }
+  
+//         testData.push(currentTest);
+//       }
+//     }
+  
+//     walk.simple(ast, {
+//       CallExpression(node) {
+//         if (node.callee.type === "MemberExpression" && 
+//             node.callee.object.name === "test" && 
+//             node.callee.property.name === "describe") {
+//           // Handle test.describe
+//           const describeOptions = node.arguments[1];
+//           if (describeOptions && describeOptions.type === "ObjectExpression") {
+//             const tagProperty = describeOptions.properties.find((prop) => prop.key.name === "tag");
+//             if (tagProperty) {
+//               describeTags = {};
+//               let tags;
+//               if (tagProperty.value.type === "ArrayExpression") {
+//                 tags = tagProperty.value.elements.map((el) => el.value);
+//               } else if (tagProperty.value.type === "Literal") {
+//                 tags = tagProperty.value.value;
+//               }
+//               parseTags(tags, describeTags);
+//             }
+//           }
+//           // Process tests within the describe block
+//           const testsBlock = node.arguments[2];
+//           if (testsBlock.type === "ArrowFunctionExpression" && testsBlock.body.type === "BlockStatement") {
+//             testsBlock.body.body.forEach(statement => {
+//               if (statement.type === "ExpressionStatement" && statement.expression.type === "CallExpression") {
+//                 processTestNode(statement.expression, describeTags);
+//               }
+//             });
+//           }
+//         } else if (node.callee.name === "test") {
+//           processTestNode(node);
+//         }
+//       },
+//     });
+  
+//     return testData;
+//   }
+function extractDataFromFile(filePath, testType) {
+    // console.log(filePath);
+    const data = fs.readFileSync(filePath, "utf-8");
+    const ast = parse(data, {
+      sourceType: "module",
+      ecmaVersion: "latest",
+      loc: true,
+      range: true,
+      tokens: true,
+    });
+    const testData = [];
+  
+    function processTestNode(node, inheritedTags = {}) {
       if (
+        node.type === 'CallExpression' &&
         node.callee.name === "test" &&
         node.arguments.length >= 2 &&
         (node.arguments[0].type === "Literal" || node.arguments[0].type === "TemplateLiteral")
       ) {
         const testName = node.arguments[0].value || node.arguments[0].quasis[0].value.raw;
-        const currentTest = { testName, functionality: "", testType, priority: "", scenarioType: "" };
-
-        if (node.arguments[1].type === "ObjectExpression") {
+        const currentTest = { 
+          testName, 
+          functionality: inheritedTags.functionality || "",
+          testType, 
+          priority: inheritedTags.priority || "", 
+          scenarioType: inheritedTags.scenarioType || "" 
+        };
+  
+        if (node.arguments[1] && node.arguments[1].type === "ObjectExpression") {
           const tagProperty = node.arguments[1].properties.find((prop) => prop.key.name === "tag");
           if (tagProperty) {
-            const tags = tagProperty.value.elements.map((el) => el.value);
+            let tags;
+            if (tagProperty.value.type === "ArrayExpression") {
+              tags = tagProperty.value.elements.map((el) => el.value);
+            } else if (tagProperty.value.type === "Literal") {
+              tags = tagProperty.value.value;
+            }
             parseTags(tags, currentTest);
           }
         }
-
+  
         testData.push(currentTest);
       }
-    },
-  });
-
-  return testData;
-}
+    }
+  
+    function processDescribeBlock(node) {
+      let describeTags = {};
+      
+      if (node.arguments[1] && node.arguments[1].type === "ObjectExpression") {
+        const tagProperty = node.arguments[1].properties.find((prop) => prop.key.name === "tag");
+        if (tagProperty) {
+          let tags;
+          if (tagProperty.value.type === "ArrayExpression") {
+            tags = tagProperty.value.elements.map((el) => el.value);
+          } else if (tagProperty.value.type === "Literal") {
+            tags = tagProperty.value.value;
+          }
+          parseTags(tags, describeTags);
+        }
+      }
+  
+      const testsBlock = node.arguments[2];
+      if (testsBlock.type === "ArrowFunctionExpression" && testsBlock.body.type === "BlockStatement") {
+        testsBlock.body.body.forEach(statement => {
+          if (statement.type === "ExpressionStatement" && statement.expression.type === "CallExpression") {
+            processTestNode(statement.expression, describeTags);
+          }
+        });
+      }
+    }
+  
+    function traverseAST(node) {
+      if (node.type === 'Program') {
+        node.body.forEach(traverseAST);
+      } else if (node.type === 'ExpressionStatement') {
+        traverseAST(node.expression);
+      } else if (node.type === 'CallExpression') {
+        if (node.callee.type === "MemberExpression" && 
+            node.callee.object.name === "test" && 
+            node.callee.property.name === "describe") {
+          processDescribeBlock(node);
+        } else if (node.callee.name === "test") {
+          processTestNode(node);
+        }
+      }
+    }
+  
+    traverseAST(ast);
+  
+    return testData;
+  }
 
 function getTestFiles(directory) {
   return glob.sync(path.join(directory, "**/*.{spec.js,spec.ts}"));
@@ -102,12 +296,13 @@ function writeDataToExcel(data, outputFile) {
   workbook.xlsx
     .writeFile(outputFile)
     .then(() => {
-      console.log(`Data successfully written to ${outputFile}`);
+      console.log(`Report generated successfully`);
     })
     .catch((error) => {
-      console.error("Error writing to Excel file", error);
+      console.error("Error geenrating report", error);
     });
 }
 
+console.log("Generating report...please wait")
 const testData = processTestFiles(testDirectory);
 writeDataToExcel(testData, "Test_Data.xlsx");
