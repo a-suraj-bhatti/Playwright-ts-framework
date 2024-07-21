b425af0b4702e56de50b6b96cb93e1fbfe07f3eb
surajbhatti <a.suraj@outlook.com>
2024-07-21 18:32:10 +0530

    adding modified and optimized report generator

--- scripts/report-generator.js
+++ scripts/report-generator.js
@@ -25,145 +25,10 @@ function parseTags(tags, currentTest) {
   });
 }
 
-// function extractDataFromFile(filePath, testType) {
-//   console.log(filePath);
-//   const data = fs.readFileSync(filePath, "utf-8");
-//   const ast = parse(data, {
-//     sourceType: "module",
-//     ecmaVersion: "latest",  // Use the latest ECMAScript version
-//     loc: true,
-//     range: true,
-//     tokens: true,
-//     jsx: true,  // Enable JSX parsing if your tests use it
-//     ecmaFeatures: {
-//       globalReturn: false,
-//       impliedStrict: true,
-//       jsx: true,
-//     },
-//     // Enable all types of statements to be parsed
-//     allowImportExportEverywhere: true,
-//     allowAwaitOutsideFunction: true,
-//     allowReturnOutsideFunction: true,
-//     allowSuperOutsideMethod: true,
-//   });
-//   const testData = [];
 
-//   walk.simple(ast, {
-//     CallExpression(node) {
-//         console.log(node);
-//       if (
-//         node.callee.name === "test" &&
-//         node.arguments.length >= 2 &&
-//         (node.arguments[0].type === "Literal" || node.arguments[0].type === "TemplateLiteral")
-//       ) {
-//         const testName = node.arguments[0].value || node.arguments[0].quasis[0].value.raw;
-//         const currentTest = { testName, functionality: "", testType, priority: "", scenarioType: "" };
-
-//         if (node.arguments[1].type === "ObjectExpression") {
-//           const tagProperty = node.arguments[1].properties.find((prop) => prop.key.name === "tag");
-//           if (tagProperty) {
-//             let tags;
-//             if (tagProperty.value.type === "ArrayExpression") {
-//               tags = tagProperty.value.elements.map((el) => el.value);
-//             } else if (tagProperty.value.type === "Literal") {
-//               tags = tagProperty.value.value;
-//             }
-//             console.log(tags)
-//             parseTags(tags, currentTest);
-//           }
-//         }
-
-//         testData.push(currentTest);
-//       }
-//     },
-//   });
-
-//   return testData;
-// }
-// function extractDataFromFile(filePath, testType) {
-//     console.log(filePath);
-//     const data = fs.readFileSync(filePath, "utf-8");
-//     const ast = parse(data, {
-//       sourceType: "module",
-//       ecmaVersion: "latest",
-//       loc: true,
-//       range: true,
-//       tokens: true,
-//     });
-//     const testData = [];
-//     let describeTags = {};
-  
-//     function processTestNode(node, inheritedTags = {}) {
-//       if (
-//         node.callee.name === "test" &&
-//         node.arguments.length >= 2 &&
-//         (node.arguments[0].type === "Literal" || node.arguments[0].type === "TemplateLiteral")
-//       ) {
-//         const testName = node.arguments[0].value || node.arguments[0].quasis[0].value.raw;
-//         const currentTest = { 
-//           testName, 
-//           functionality: inheritedTags.functionality || "",
-//           testType, 
-//           priority: inheritedTags.priority || "", 
-//           scenarioType: inheritedTags.scenarioType || "" 
-//         };
-  
-//         if (node.arguments[1].type === "ObjectExpression") {
-//           const tagProperty = node.arguments[1].properties.find((prop) => prop.key.name === "tag");
-//           if (tagProperty) {
-//             let tags;
-//             if (tagProperty.value.type === "ArrayExpression") {
-//               tags = tagProperty.value.elements.map((el) => el.value);
-//             } else if (tagProperty.value.type === "Literal") {
-//               tags = tagProperty.value.value;
-//             }
-//             parseTags(tags, currentTest);
-//           }
-//         }
-  
-//         testData.push(currentTest);
-//       }
-//     }
-  
-//     walk.simple(ast, {
-//       CallExpression(node) {
-//         if (node.callee.type === "MemberExpression" && 
-//             node.callee.object.name === "test" && 
-//             node.callee.property.name === "describe") {
-//           // Handle test.describe
-//           const describeOptions = node.arguments[1];
-//           if (describeOptions && describeOptions.type === "ObjectExpression") {
-//             const tagProperty = describeOptions.properties.find((prop) => prop.key.name === "tag");
-//             if (tagProperty) {
-//               describeTags = {};
-//               let tags;
-//               if (tagProperty.value.type === "ArrayExpression") {
-//                 tags = tagProperty.value.elements.map((el) => el.value);
-//               } else if (tagProperty.value.type === "Literal") {
-//                 tags = tagProperty.value.value;
-//               }
-//               parseTags(tags, describeTags);
-//             }
-//           }
-//           // Process tests within the describe block
-//           const testsBlock = node.arguments[2];
-//           if (testsBlock.type === "ArrowFunctionExpression" && testsBlock.body.type === "BlockStatement") {
-//             testsBlock.body.body.forEach(statement => {
-//               if (statement.type === "ExpressionStatement" && statement.expression.type === "CallExpression") {
-//                 processTestNode(statement.expression, describeTags);
-//               }
-//             });
-//           }
-//         } else if (node.callee.name === "test") {
-//           processTestNode(node);
-//         }
-//       },
-//     });
-  
-//     return testData;
-//   }
 function extractDataFromFile(filePath, testType) {
     // console.log(filePath);
+    const fileName = path.basename(filePath);
     const data = fs.readFileSync(filePath, "utf-8");
     const ast = parse(data, {
       sourceType: "module",
@@ -182,7 +47,8 @@ function extractDataFromFile(filePath, testType) {
         (node.arguments[0].type === "Literal" || node.arguments[0].type === "TemplateLiteral")
       ) {
         const testName = node.arguments[0].value || node.arguments[0].quasis[0].value.raw;
-        const currentTest = { 
+        const currentTest = {
+          fileName, 
           testName, 
           functionality: inheritedTags.functionality || "",
           testType, 
@@ -282,6 +148,7 @@ function writeDataToExcel(data, outputFile) {
   const worksheet = workbook.addWorksheet("Test Data");
 
   worksheet.columns = [
+    { header: "File Name", key: "fileName", width: 30 },  
     { header: "Test Name", key: "testName", width: 50 },
     { header: "Functionality", key: "functionality", width: 30 },
     { header: "Test Type", key: "testType", width: 15 },
@@ -299,7 +166,7 @@ function writeDataToExcel(data, outputFile) {
       console.log(`Report generated successfully`);
     })
     .catch((error) => {
-      console.error("Error geenrating report", error);
+      console.error("Error generating report", error);
     });
 }
 
