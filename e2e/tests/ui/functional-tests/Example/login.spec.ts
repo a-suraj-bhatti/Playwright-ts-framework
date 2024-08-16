d204168f44033416943cd4fdca56a820eea04b8e
surajbhatti <a.suraj@outlook.com>
2024-08-16 18:29:55 +0530

    made some tag changes

--- e2e/tests/ui/functional-tests/Example/login.spec.ts
+++ e2e/tests/ui/functional-tests/Example/login.spec.ts
@@ -4,12 +4,21 @@ test.describe(
   "There are two tests",
   {
     annotation: { type: "jira issue", description: "This is a test description" },
-    tag: ["@team_test", "@sprint_2024.1.2", "@us_SAK-133","@testcase_777"],
+    tag: ["@team_test", "@sprint_2024.1.2", "@us_SAK-133", "@testcase_777"],
   },
   () => {
     test(
       "Verify that the user is able to login to the application",
-      { tag: ["@positive", "@priority_high", "@functionality_descoverride", "@team_ovvertiode","@us_SAK-1over","@testcase_666"] },
+      {
+        tag: [
+          "@positive",
+          "@priority_high",
+          "@functionality_descoverride",
+          "@team_ovvertiode",
+          "@us_SAK-1over",
+          "@testcase_666",
+        ],
+      },
       async ({ loginPage, homePage }) => {
         await loginPage.visit();
         await loginPage.login();
